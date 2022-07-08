79ca7d95091794a06d99d1bce0c2904e3e3f3a1b
wiktoriahet <wiktoriaspyc@gmail.com>
2022-07-08 17:16:23 +0200

    updated class User and added classes PasswordService and WritingService

--- src/com/pomodoro/User.java
+++ src/com/pomodoro/User.java
@@ -6,7 +6,10 @@ import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import java.util.UUID;
 
-public class User {
+public class User{
+
+    WritingService writingService = new WritingService();
+    PasswordService passwordService = new PasswordService();
 
     //wybiera uzytkownika z istniejacych, jesli nie istnieje to daje mozliwosc utworzenia nowego
     public void chooseUser(){
@@ -18,19 +21,18 @@ public class User {
 
         printUsers();//lista uzytkownikow. czy konieczna pozniej???
 
-        userChosen = typeUserName();
-        password = typePassword();
-        isPassCorrect = isPasswordCorrect(password, userChosen);
+        userChosen = writingService.typeUserName();
 
-        if(!doesntExist(userChosen)&&isPassCorrect){
+        if(!doesntExist(userChosen)){
+            password = writingService.typePassword();
+            isPassCorrect = passwordService.isPasswordCorrect(password, userChosen);
+            if(isPassCorrect) {
 
-            System.out.println("Wybrano uzytkownika " + userChosen);
-            userMenu.userMenuStartPage(userChosen);
-
-        } else if(!doesntExist(userChosen) && !isPassCorrect){
-
-            System.out.println("Bledne haslo");
-            chooseUser();
+                System.out.println("Wybrano uzytkownika " + userChosen);
+                userMenu.userMenuStartPage(userChosen);
+            } else {
+                System.out.println("Bledne haslo");
+                chooseUser();}
 
         } else {
 
@@ -50,6 +52,112 @@ public class User {
 
     }
 
+    //dodaje nowego usera
+    public void addUser() {
+
+        try {
+            Date thisDate = new Date();
+            String userName;
+            String password;
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
+            Statement statement = connection.createStatement();
+
+            userName = writingService.typeUserName();
+
+            if(doesntExist(userName)){
+                password = writingService.typePassword();
+
+                String uniqueID = UUID.randomUUID().toString();
+                statement.executeUpdate("INSERT INTO user " + "VALUES('" + uniqueID + "', '" + userName + "', '" + password + "', '" + thisDate + "')");
+
+                System.out.println("Utworzono nowego uzytkownika: " + userName);
+
+            } else {
+                System.out.println("Podana nazwa juz istnieje");
+                addUser();
+            }
+
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addUser(String name){
+        try {
+            Date thisDate = new Date();
+            String uniqueID = UUID.randomUUID().toString();
+            String password;
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
+            Statement statement = connection.createStatement();
+
+            password = writingService.typePassword();
+
+            statement.executeUpdate("INSERT INTO user " + "VALUES('" + uniqueID + "', '" + name + "', '" + password + "', '" + thisDate + "')");
+
+            System.out.println("Utworzono nowego uzytkownika: " + name);
+
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //usuwa uzytkownika
+    public void deleteUser(){
+
+        String userName;
+        String password;
+
+        userName = writingService.typeUserName();
+        password = writingService.typePassword();
+
+        if(!doesntExist(userName)&&passwordService.isPasswordCorrect(password, userName)){
+            try {
+                String id;
+                String query;
+
+                id = getID(userName);
+                query = "delete from user where id = ?";
+
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
+                PreparedStatement preparedStmt = connection.prepareStatement(query);
+                preparedStmt.setString(1,id);
+                preparedStmt.execute();
+
+                System.out.println("Usunieto uzytkownika uzytkownika: " + userName);
+
+            } catch (
+                    Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Nie ma takiego uzytkownika badz podane haslo jest bledne");
+        }
+    }
+
+    //wypluwa id nadane w bazie danych
+    private static String getID(String userName){
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
+            Statement statement = connection.createStatement();
+            ResultSet resultSetID = statement.executeQuery("select * from user");
+
+            while (resultSetID.next()) {
+                if (resultSetID.getString("UserName").equalsIgnoreCase(userName)) {
+                    return resultSetID.getString("id");
+                }
+            }
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+
+        return "Brak uzytkownika w bazie danych";
+    }
     //drukuje wszystkich userów
     private static void printUsers() {
 
@@ -73,65 +181,7 @@ public class User {
         }
     }
 
-    //dodaje nowego usera
-    public void addUser() {
-
-        try {
-            Date thisDate = new Date();
-            String userName;
-            String password;
-
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
-            Statement statement = connection.createStatement();
-
-            userName = typeUserName();
-
-            if(doesntExist(userName)){
-                password = typePassword();
-
-                String uniqueID = UUID.randomUUID().toString();
-                statement.executeUpdate("INSERT INTO user " + "VALUES('" + uniqueID + "', '" + userName + "', '" + password + "', '" + thisDate + "')");
-
-                System.out.println("Utworzono nowego uzytkownika: " + userName);
-
-            } else {
-                System.out.println("Podana nazwa juz istnieje");
-                addUser();
-            }
-
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void addUser(String name){
-        try {
-
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
-
-            Statement statement = connection.createStatement();
-
-            System.out.println("Utworz haslo: ");
-            Scanner input2 = new Scanner(System.in);
-            String password = input2.nextLine();
-
-            Date thisDate = new Date();
-
-            String uniqueID = UUID.randomUUID().toString();
-
-                ResultSet resultSetCount = statement.executeQuery("select * from user");
-                statement.executeUpdate("INSERT INTO user " + "VALUES('" + uniqueID + "', '" + name + "', '" + password + "', '" + thisDate + "')");
-            System.out.println("Utworzono nowego uzytkownika: " + name);
-
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     //sprawdza czy user istnieje
-
     private static boolean doesntExist(String name) {
 
         try {
@@ -151,105 +201,5 @@ public class User {
         }
         return true;
     }
-
-    //usuwa uzytkownika
-    public void deleteUser(){
-
-
-        System.out.println("Wpisz nazwe uzytkownika, ktorego chcesz usunac");
-        Scanner input = new Scanner(System.in);
-        String userName;
-        userName = input.nextLine();
-        System.out.println("Wpisz haslo");
-        Scanner input2 = new Scanner(System.in);
-        String password;
-        password = input2.nextLine();
-        //isPasswordCorrect(password, userName);
-
-        if(!doesntExist(userName)&&isPasswordCorrect(password, userName)){
-            try {
-
-
-                String id = getID(userName);
-                String query = "delete from user where id = ?";
-                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
-                PreparedStatement preparedStmt = connection.prepareStatement(query);
-                preparedStmt.setString(1,id);
-                preparedStmt.execute();
-                System.out.println("Usunieto uzytkownika uzytkownika: " + userName);
-
-
-
-
-            } catch (
-                    Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println("Nie ma takiego uzytkownika badz podane haslo jest bledne");
-        }
-    }
-
-    //wypluwa id nadane w bazie danych
-    private static String getID(String userName){
-
-
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
-            Statement statement = connection.createStatement();
-            ResultSet resultSetID = statement.executeQuery("select * from user");
-
-            while (resultSetID.next()) {
-                if (resultSetID.getString("UserName").equalsIgnoreCase(userName)) {
-                    return resultSetID.getString("id");
-                }
-            }
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }
-
-        return "0";
-    }
-
-    //sprawdza poprawnosc hasla
-
-    private static boolean isPasswordCorrect(String password, String name){
-
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pomodorodb", "toriahem", "Gk6tnmcj!");
-            Statement statement = connection.createStatement();
-            ResultSet resultSetPassword = statement.executeQuery("select * from user");
-
-            while (resultSetPassword.next()) {
-                if (resultSetPassword.getString("UserName").equalsIgnoreCase(name)) {
-                    return resultSetPassword.getString("password").equals(password);
-                }
-            }
-
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }
-
-        return false;
-
-    }
-
-    public String typeUserName(){
-        System.out.println("Wpisz nazwe uzytkownika");
-        Scanner input = new Scanner(System.in);
-        String userName = input.nextLine();
-        return userName;
-    }
-
-    public String typePassword(){
-        System.out.println("Wpisz haslo");
-        Scanner input = new Scanner(System.in);
-        String password = input.nextLine();
-        return password;
-    }
-
-
 }
 
